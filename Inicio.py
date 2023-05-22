bce4067f623a0c480627e54fd4c861a37a80b7bd
Vinicius Baldan Herrera <viniciusbherrera@outlook.com>
2023-05-22 16:23:48 -0300

    Create Inicio.py

---
+++ Inicio.py
@@ -0,0 +1,6 @@
+##Inicio do código de trabalho da equipe## Aline Tosatti, Eduardo Lubian, Rogerio **, Vinicius Herrera
+#Atenção!# Este código utiliza da biblioteca kivy e precisa ser compilado no máximo até o python 3.10
+
+from kivy.app import App 
+from kivy.uix.widget import Widget
+
