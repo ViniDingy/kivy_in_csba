d67bc35f2795be85586e05aef4c9e81a8f2d82bb
Vinicius Baldan Herrera <viniciusbherrera@outlook.com>
2023-05-24 14:06:42 -0300

    Adding base structures

--- main/Inicio.py
+++ main/Inicio.py
@@ -2,5 +2,31 @@
 #Atenção!# Este código utiliza da biblioteca kivy e precisa ser compilado no máximo até o python 3.10
 
 from kivy.app import App 
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 
+class Tela(BoxLayout):
+    pass
+
+class Nosso_jogoApp(App):
+    def build(self):
+        return Tela()
+    
+Nosso_jogoApp().run()
+
+
+
+
+
+
+
+"""                   /^--^\     /^--^\     /^--^\
+                      \____/     \____/     \____/
+                     /      \   /      \   /      \
+                    |        | |        | |        |
+                     \__  __/   \__  __/   \__  __/
+|^|^|^|^|^|^|^|^|^|^|^|^\ \^|^|^|^/ /^|^|^|^|^\ \^|^|^|^|^|^|^|^|^|^|^|^|
+| | | | | | | | | | | | |\ \| | |/ /| | | | | | \ \ | | | | | | | | | | |
+########################/ /######\ \###########/ /#######################
+| | | | | | | | | | | | \/| | | | \/| | | | | |\/ | | | | | | | | | | | |
+|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|
+"""
