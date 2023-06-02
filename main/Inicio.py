efec7d9830d33de2a748c98090f8081d0d44bf32
Vinicius Baldan Herrera <viniciusbherrera@outlook.com>
2023-06-02 15:47:35 -0300

    Alterações

--- main/Inicio.py
+++ main/Inicio.py
@@ -1,15 +1,28 @@
 ##Inicio do código de trabalho da equipe## Aline Tosatti, Eduardo Lubian, Rogerio **, Vinicius Herrera
 #Atenção!# Este código utiliza da biblioteca kivy e precisa ser compilado no máximo até o python 3.10
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
 
 from kivy.app import App 
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class Tela(BoxLayout):
+class Gerenciador(ScreenManager):
+    pass
+
+class Tela(Screen):
+    imagem_fundo = 'Fundo.jpg'
+
+    ##Definição de Fontes##
+    pasta_fontes = 'Marbella Army.otf'
     pass
 
 class Nosso_jogoApp(App):
     def build(self):
-        return Tela()
+        Window.size = (1000, 1000)
+        return Gerenciador()
     
 Nosso_jogoApp().run()
 
