a9ec77a905a26b685de8623d5c09778eb4855656
Vinicius Baldan Herrera <viniciusbherrera@outlook.com>
2023-06-14 03:05:12 -0300

    Mudanças vini

--- main/Inicio.py
+++ main/Inicio.py
@@ -6,9 +6,9 @@ Config.set('graphics', 'resizable', False)
 from kivy.app import App 
 from kivy.uix.button import Button
 from kivy.core.window import Window
-from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
+from kivy.core.audio import SoundLoader
 
 class Jogo:
     def __init__(self, **kwargs):
@@ -18,23 +18,67 @@ class Jogo:
         self.navios_restantesp1 = 0
         self.p2 = self.tabuleiro
         self.navios_restantesp2 = 0
+        self.log_de_jogop1 = []
+        self.log_de_jogop2 = []
+
+    def retorna_p1(self):
+        return self.p1
+    
+    def identifica_posicionamento(self, log_de_jogo):
+        pass
 
     def criar_tabuleiro(self):
         Tabuleiro = []
         for linha in range(10):
-            lin = []
+            linha = []
             for coluna in range(10):
-                 lin.append(0)
-            Tabuleiro.append(lin)
+                 linha.append(0)
+            Tabuleiro.append(linha)
         return Tabuleiro
+    
+    def acao_botão_p1(self, button):
+        print("apertou o botão")
+        button_id = button.text
+        print(button_id)
+        bt_id_1 = int(button_id[0])
+        bt_id_2 = int(button_id[2])
+        button.background_color = (0, 0, 1, 1)
+        self.p1[(bt_id_1)][(bt_id_2)] = 1
+        for i in range(len(self.p1)):
+            for j in range(len(self.p1[i])):
+                print('[%1d]'%self.p1[i][j], end ='')
+            print("\n")
+        self.navios_restantesp1 += 1
+        print(self.navios_restantesp1)
+        log_atual = []
+        log_atual.append(bt_id_1)
+        log_atual.append(bt_id_2)
+        self.log_de_jogop1.append(log_atual)
+        
     
     # Começar por Aqui
 
     def criar_gridtabuleiro(self):
-        for i in range(self.p1):
-            for j in range(self.p1[i]):
-                bt = Button(id = f'bt{i},{j}',text = f'{i},{j}')
-                self.ids['gradtab1'].add_widget(bt)
+        for linha in range(len(self.p1)):
+            for coluna in range(len(self.p1[linha])):
+                bt = Button(
+                    text=f"{linha},{coluna}",
+                    size_hint=(0.2, 0.2),
+                    font_size=24,
+                    on_press = self.acao_botão_p1
+                )
+                bt.id = f"{linha},{coluna}"
+                self.ids.gradtab1.add_widget(bt)
+
+''' def posicionar_navios(self, button):
+        if self.navios_restantesp1 < 1:
+            self.acao_botão_p1
+        else:
+            if self.navios_restantes < 8 and self.navios_restantes %2 == 0:
+                button_id = button.text
+                bt_id_1 = int(button_id[0])
+                bt_id_2 = int(button_id[2])
+                if self.log_de_jogop1[0] - (bt_id_1 - bt_id_2):'''
 
 class Gerenciador(ScreenManager):
     pass
@@ -42,12 +86,9 @@ class Gerenciador(ScreenManager):
 class Menu(Screen, Jogo):
     def __init__(self, **kwargs):
         super().__init__()
-
-    def Inicio(self):
-        print(self.tabuleiro)
-        self.criar_gridtabuleiro()
-
+        self.som = SoundLoader.load("The Majestic Valkyrie.wav")
     imagem_fundo = 'Fundo.jpg'
+    
 
     ##Definição de Fontes##
     pasta_fontes = 'Marbella Army.otf'
@@ -58,8 +99,14 @@ class Player1(Screen, Jogo):
         super().__init__()
 
     def Inicio(self):
-        print(self.tabuleiro)
+        tabuleiro_de_p1 = Jogo().retorna_p1()
+        for i in range(len(tabuleiro_de_p1)):
+            for j in range(len(tabuleiro_de_p1[i])):
+                print('[%1d]'%tabuleiro_de_p1[i][j], end ='')
+            print("\n")
         self.criar_gridtabuleiro()
+        
+        
 
 class Player2(Screen, Jogo):
     def __init__(self, **kwargs):
@@ -70,9 +117,9 @@ class Nosso_jogoApp(App):
         Window.size = (1000, 1000)
         return Gerenciador()
     
-    
+print("##Iniciando##")
 Nosso_jogoApp().run()
-print('olá')
+print('##Finalizando##')
 
 
 
--- main/Testevini.py
+++ main/Testevini.py
@@ -1,3 +1,45 @@
-x  = 0
-y = 8
-print(f'x + y = {x+y}')
+a = 0
+b = 0
+c = 0
+d = 5
+n_linhas = int(input("Insira quantidade de linhas na matriz: "))
+while n_linhas <= 0:
+    print("Você só pode usar numeros maiores que 0")
+    lista_user = int(input("insira o Tamanho novamente: "))
+tam_lista  = int(input("Insira o tamanho da lista: "))
+lista_user = 0
+Matriz = []
+Lista = []
+while tam_lista <= 0:
+    print("Você só pode usar numeros maiores que 0")
+    tam_lista = int(input("Insira o Tamanho novamente: "))
+for i in range(tam_lista):
+    lista_user = int(input("Insira valores inteiros entre -10 e 5: "))
+    while lista_user < -10:
+        print("Você só pode usar numeros maiores que -10")
+        lista_user = int(input("Tente inserir os valores inteiros entre -10 e 5 novamente: "))
+    while lista_user > 5:
+        print("Você só pode usar numeros maiores que 5")
+        lista_user = int(input("Tente inserir os valores inteiros entre -10 e 5 novamente: "))
+    Lista.append(lista_user)
+print("----Lista----")
+for i in range(len(Lista)):
+    print(Lista[i])
+print("--------")
+Matriz.append(Lista)
+print("----Matriz----")
+for i in range(len(Lista)):
+    l = []
+    b += 1
+    for j in range(n_linhas):
+        a = Lista[j]
+        c = a * b
+        l.append(c)
+    Matriz.append(l)
+print(Matriz)
+for i in range(len(Matriz)):
+    for j in range(n_linhas):
+        if j == i:
+            if Matriz[i][j-1] < d:
+                d = Matriz[i][j-1]
+print("O menor valor acima da diagonal é ",d)
